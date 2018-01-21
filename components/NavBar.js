13d22c0ad323f1617bc89af958dea336b34dc6b9
Dev <developer@cactus.ws>
2018-01-21 13:51:41 -0300

    Fixed lcurrent on restaurants

--- components/NavBar.js
+++ components/NavBar.js
@@ -19,7 +19,7 @@ function Menu ({ menu }) {
 
 function MenuItem ({ menuItem }) {
   return (
-    <li><Link href={ menuItem.href } ><a className="current">{menuItem.html}</a></Link>
+    <li><Link href={ menuItem.href } ><a >{menuItem.html}</a></Link>
     {(() => {
       if(menuItem.children instanceof Array) {
         return (
