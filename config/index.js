c78e21e255d91e105142b77827f1be1232f2b5df
Dev <developer@cactus.ws>
2018-02-21 16:26:24 -0300

    Testing domain

--- config/index.js
+++ config/index.js
@@ -14,6 +14,7 @@ export function configSite(req) {
 	if(req) {
 		console.log("headerss",req.headers)
 		console.log("headers react app",req.headers.react_app_site)		
+    console.log("headers react app",req.hostname)   
 	}
 
     const site  =  	('undefined' !== typeof window) ? 
