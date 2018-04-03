66dcaa55ea261217dff7b61bf796dcd55dbe730c
Dario Miñones <dariominones@cactus.ws>
2018-04-02 21:18:12 -0300

    configuration

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 import sites from './sites';
 
-const { NODE_ENV } = ('undefined' !== typeof window) ? window.env : process.env
+const { NODE_ENV, REACT_APP_SITE } = ('undefined' !== typeof window) ? window.env : process.env
 
 const environment = NODE_ENV || 'development';
 
@@ -10,24 +10,21 @@ export default { 
 	...config.config
 }
 
-export function configSite(req) {
-	if(req) {
-		console.log("headerss",req.headers)
-		console.log("headers react app",req.headers.react_app_site)		
-    console.log("headers react app",req.hostname)   
-	}
-
-    const site  =  	('undefined' !== typeof window) ? 
-                    (('undefined' !== typeof window.env) ? window.env.REACT_APP_SITE : null) :
-                  	(('undefined' !== typeof req) ? req.headers.react_app_site : null)
-
-    return sites[site || 'bolivia'];
+export function configSite() {
+    const site  =  	REACT_APP_SITE ? REACT_APP_SITE : 'bolivia';
+    console.log("SITE ---->", site);
+    return sites[site];
 }
 
-export function getClientConfigSite() {	
+export function getClientConfigSite() {
+    //Getting config from process.env
+    return configSite();
+
+    /*
     const site  =  	('undefined' !== typeof window) ? 
                     (('undefined' !== typeof window.env) ? window.env.REACT_APP_SITE : null) :
                   	null
 
     return sites[site || 'bolivia'];
+    */
 }
