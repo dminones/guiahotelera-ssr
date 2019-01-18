c13575d234e5c019b18d042ad511babd0a10b46a
Your Name <dminones@gmail.com>
2019-01-18 12:50:59 -0300

    Adding post page

--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,31 @@
+import React, { Component } from 'react'
 import Layout from '../components/MyLayout.js'
+import { Post } from '../containers'
+import { getPost } from '../data/'
+import {configSite} from '../config'
+export default class extends Component {
 
-export default (props) => (
-    <Layout>
-       <h1>{props.url.query.title}</h1>
-       <p>This is the blog post content.</p>
-    </Layout>
-)
+	static async getInitialProps({ req, query }) {
+		let post;
+		try {
+			post = await getPost(query.slug);
+			console.log(item)
+		} catch(e) {
+			console.log(e)
+		}
+		const site = configSite(req);
+		return {
+			post,
+			site
+		}
+    }
+    
+    render() {
+        const { post, site } = this.props;
+        return (
+            <Layout site={site}>
+               <Post site={site} post={post} />
+            </Layout>
+        )
+    }
+}
